c3ef7250bedcabfbcd2c7583f9252b1ef4b0b890
shiva kumar <shivakumar.v@mangomassmedia.com>
2025-08-05 14:48:22 +0530

    Updated content or features

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/shivakumar-vanam/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 })
